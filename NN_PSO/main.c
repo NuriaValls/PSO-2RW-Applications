d921fcf576924f82d7a3c0686235657091ca7686
praat6 <43272208+praat6@users.noreply.github.com>
2019-05-27 23:32:10 +0200

    h

--- NN_PSO/main.c
+++ NN_PSO/main.c
@@ -2,8 +2,6 @@
 
 #include "logica.h"
 
-#include "neural_net.c"
-
 
 int select_velocity_method = 3;
 int convergence_factor = 100;
@@ -24,6 +22,13 @@ int main(int argc, char **argv) {
     int not_converged = 1;
     double best_fit_compare;
 
+    Matrix yhat;
+    Matrix data;
+    Matrix y;
+    Matrix test;
+    Matrix y_test;
+    Matrix yhat_test;
+
     srand(time(NULL));
 
     if (!checkProgramArguments(argc)) {
@@ -33,22 +38,19 @@ int main(int argc, char **argv) {
 
     config = readConfigFile(argv[1]);
 
-    //Matrix data = MAT_create(ROWS_DATA, COLS_DATA);
-    //Matrix y = MAT_create(data.rows, 1);
-    //data = X_train();
-    //y = y_train(data);
+    data = X_train();
+    y = y_train(data);
 
-    createInitialPopulation(config, &swarm);
+    test = X_train();
+    y_test = y_train(test);
+
+    createInitialPopulation(config, &swarm, data, y);
 
 
     while (not_converged) {
-    //while (iter > 0) {
 
-        getFitValues(config, &swarm);//data, y);
+        getFitValues(config, &swarm, data, y);
 
-        sprintf(msg, "I: %d, Best fit value: %f, train_acc = %f, val_acc = %f\n", swarm.iterations, swarm.best_fit, swarm.best_train_acc, swarm.best_val_acc);//,swarm.best_params[0],swarm.best_params[1],swarm.best_params[2],swarm.best_params[3],swarm.best_params[4],swarm.best_params[5],swarm.best_params[6],swarm.best_params[7],swarm.best_params[8]);
-                //swarm.iterations, swarm.best_fit, swarm.best_params[0],swarm.best_params[1],swarm.best_params[2],swarm.best_params[3],swarm.best_params[4],swarm.best_params[5],swarm.best_params[6],swarm.best_params[7],swarm.best_params[8]);
-        debug(msg);
 
         best_fit_compare = trunc(swarm.best_fit * 100000000);
 
@@ -64,14 +66,27 @@ int main(int argc, char **argv) {
             count_convergence++;
         } else {
             count_convergence = 0;
-            sprintf(msg, "\t%f, %f\n",best_fit_local, best_fit_compare);
-            debug(msg);
             best_fit_local = best_fit_compare;
         }
 
         if (count_convergence == convergence_factor) {
             not_converged = 0;
         }
+
+        yhat = forward_pass(data, swarm.best_params);
+        float b_train_acc = accuracy(y, yhat);
+
+        yhat_test = forward_pass(test, swarm.best_params);
+
+        float val_loss = mse_loss(y_test, yhat_test);
+        float b_val_acc = accuracy(y_test, yhat_test);
+
+        swarm.best_train_acc = b_train_acc;
+        swarm.best_val_acc = b_val_acc;
+
+        sprintf(msg, "I: %d, Best fit value: %f, train_acc = %f, val_loss = %f, val_acc = %f\n", swarm.iterations, swarm.best_fit, swarm.best_train_acc, val_loss, swarm.best_val_acc);//,swarm.best_params[0],swarm.best_params[1],swarm.best_params[2],swarm.best_params[3],swarm.best_params[4],swarm.best_params[5],swarm.best_params[6],swarm.best_params[7],swarm.best_params[8]);
+            //swarm.iterations, swarm.best_fit, swarm.best_params[0],swarm.best_params[1],swarm.best_params[2],swarm.best_params[3],swarm.best_params[4],swarm.best_params[5],swarm.best_params[6],swarm.best_params[7],swarm.best_params[8]);
+        debug(msg);
 
         iter--;
         swarm.iterations++;
